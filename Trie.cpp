7bb9a9138fae5d637e19617a41b0b24d9825f240
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2020-12-11 22:53:06 +0700

    5 ham dau

--- Trie.cpp
+++ Trie.cpp
@@ -1,23 +1,51 @@
 #include "Trie.h"
 
-Trie* get(Trie* p, string key, int d);
-Trie* findNode(Trie* p, string key);
-Trie* put(Trie* p, string key, string val, int d);
-void Insertion(Trie* root, string key, string val);
-void collect(Trie* p, string prefix, int d);
-void keysWithPrefix(Trie* root, string prefix) {
-    Trie* p = get(root, prefix, 0);
+Node* get(Node* p, string key, int d){
+    if (p == NULL) return NULL;
+    if (d == key.length()) return p;
+    int c = key[d];
+    return get(p->next[c], key, d + 1);
+}
+Node* findNode(Node* p, string key){
+    p = get(p, key, 0);
+    return (p && !p->value.empty()) ? p : NULL;
+}
+Node* put(Node* p, string key, string val, int d){
+    if (p == NULL) {
+        p = new Node;
+        for (int i = 0; i < 26; i++) p->next[i] = NULL;
+    }
+    if (d == key.length()) {
+        p->value = val;
+        return p;
+    }
+    int c = key[d];
+    p->next[c] = put(p->next[c], key, val, d + 1);
+    return p;
+}
+void Insertion(Node* root, string key, string val){
+    root = put(root, key, val, 0);
+}
+void collect(Node* p, string prefix, int d){
+    if (p == NULL) return;
+    if (!p->value.empty())
+        cout << p->value << " " << prefix << endl;
+    for (int c = 0; c < 26; c++)
+        collect(p->next[c], prefix + char(c), d + 1);
+}
+void keysWithPrefix(Node* root, string prefix) {
+    Node* p = get(root, prefix, 0);
     int d = prefix.length();
     collect(p, prefix, d);
 }
-int search(Trie* p, string s, int d, int length){
+int search(Node* p, string s, int d, int length){
     if (p == NULL) return length;
     if (!p->value.empty()) length = d;
     if (d == s.length()) return length;
     int c = s[d];
     return search(p->next[c], s, d + 1, length);
 }
-string longestPrefixOf(Trie* root, string s){
+string longestPrefixOf(Node* root, string s){
     int length = search(root, s, 0, 0);
     string result;
     for (int i = 0; i < length; i++) {
@@ -25,7 +53,7 @@ string longestPrefixOf(Trie* root, string s){
     }
     return result;
 }
-Trie* del(Trie* p, string key, int d){
+Node* del(Node* p, string key, int d){
     if (p == NULL) return NULL;
     if (d == key.length())
         p->value = "";
@@ -39,6 +67,6 @@ Trie* del(Trie* p, string key, int d){
     delete p;
     return NULL;
 }
-void Deletion(Trie* root, string key){
-    Trie* root = del(root, key, 0);
+void Deletion(Node* root, string key){
+    Node* root = del(root, key, 0);
 }
--- Trie.h
+++ Trie.h
@@ -4,21 +4,21 @@
 #include<iostream>
 using namespace std;
 
-struct Trie
+struct Node
 {
     string value;
-    Trie* next[26];
+    Node* next[26];
 };
 
-Trie* get(Trie* p, string key, int d);
-Trie* findNode(Trie* p, string key);
-Trie* put(Trie* p, string key, string val, int d);
-void Insertion(Trie* root, string key, string val);
-void collect(Trie* p, string prefix, int d);
-void keysWithPrefix(Trie* root, string prefix);
-int search(Trie* p, string s, int d, int length); 
-string longestPrefixOf(Trie* root, string s);
-Trie* del(Trie* p, string key, int d);
-void Deletion(Trie* root, string key); 
+Node* get(Node* p, string key, int d);
+Node* findNode(Node* p, string key);
+Node* put(Node* p, string key, string val, int d);
+void Insertion(Node* root, string key, string val);
+void collect(Node* p, string prefix, int d);
+void keysWithPrefix(Node* root, string prefix);
+int search(Node* p, string s, int d, int length); 
+string longestPrefixOf(Node* root, string s);
+Node* del(Node* p, string key, int d);
+void Deletion(Node* root, string key); 
 
 #endif
