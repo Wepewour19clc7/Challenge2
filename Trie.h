54606e2a42780bf9e74d1a602337b59403ecd6cd
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2020-12-11 18:35:36 +0700

    them ham

--- Trie.h
+++ Trie.h
@@ -1,9 +1,24 @@
 #ifndef _TRIE_H_
 #define _TRIE_H_
 
+#include<iostream>
+using namespace std;
+
 struct Trie
 {
-    
+    string value;
+    char** next[26];
 };
+
+Trie* get(Trie* p, string key, int d);
+Trie* findNode(Trie* p, string key);
+Trie* put(Trie* p, string key, string val, int d);
+void Insertion(Trie* root, string key, string val);
+void collect(Trie* p, string prefix, int d);
+void keysWithPrefix(Trie* root, string prefix);
+int search(Trie* p, string s, int d, int length); 
+string longestPrefixOf(Trie* root, string s);
+Trie* del(Trie* p, string key, int d);
+void Deletion(Trie* root, string key); 
 
 #endif
