1494cc3126978f15f9d484eba59b319a8e32c3df
Wepewour19clc7 <63486066+Wepewour19clc7@users.noreply.github.com>
2020-12-11 22:19:34 +0700

    Add 5 hàm cuối

--- Trie.cpp
+++ Trie.cpp
@@ -5,8 +5,40 @@ Trie* findNode(Trie* p, string key);
 Trie* put(Trie* p, string key, string val, int d);
 void Insertion(Trie* root, string key, string val);
 void collect(Trie* p, string prefix, int d);
-void keysWithPrefix(Trie* root, string prefix);
-int search(Trie* p, string s, int d, int length); 
-string longestPrefixOf(Trie* root, string s);
-Trie* del(Trie* p, string key, int d);
-void Deletion(Trie* root, string key); 
+void keysWithPrefix(Trie* root, string prefix) {
+    Trie* p = get(root, prefix, 0);
+    int d = prefix.length();
+    collect(p, prefix, d);
+}
+int search(Trie* p, string s, int d, int length){
+    if (p == NULL) return length;
+    if (!p->value.empty()) length = d;
+    if (d == s.length()) return length;
+    int c = s[d];
+    return search(p->next[c], s, d + 1, length);
+}
+string longestPrefixOf(Trie* root, string s){
+    int length = search(root, s, 0, 0);
+    string result;
+    for (int i = 0; i < length; i++) {
+        result.push_back(s[i]);
+    }
+    return result;
+}
+Trie* del(Trie* p, string key, int d){
+    if (p == NULL) return NULL;
+    if (d == key.length())
+        p->value = "";
+    else {
+        int c = key[d];
+        p->next[c] = del(p->next[c], key, d + 1);
+    }
+    if (p->value.empty()) return p;
+    for (int c = 0; c < 26; c++)
+        if (p->next[c] != NULL) return p;
+    delete p;
+    return NULL;
+}
+void Deletion(Trie* root, string key){
+    Trie* root = del(root, key, 0);
+}
